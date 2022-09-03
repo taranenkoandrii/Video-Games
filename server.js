d38dc4e32892bea269d6ad9a1831638cfaa8924f
Taranenko Andrii <taranenkoandrii08@gmail.com>
2022-09-04 01:51:42 +0300

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const path = require('path');
 const app = express();
-app.use(express.static(__dirname + '/dist/video-games'));
+app.use(express.static(__dirname + '/dist/video_games'));
 app.get('/*', function(req,res) {
 res.sendFile(path.join(__dirname+
-'/dist/video-games/index.html'));});
+'/dist/video_games/index.html'));});
 app.listen(process.env.PORT || 8080);
